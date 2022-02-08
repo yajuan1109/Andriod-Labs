c1b6a08055eafd5cc05233ad24fde624f4229d47
Yajuan Wang <yajuanwang1109@gmail.com>
2022-02-08 11:18:21 -0500

    step 2

--- app/src/main/java/com/cst2335/yaju0001/MainActivity.java
+++ app/src/main/java/com/cst2335/yaju0001/MainActivity.java
@@ -18,7 +18,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_relative);
+        setContentView(R.layout.activity_main_grid);
 
         clickHere = findViewById(R.id.button);
         clickHere.setOnClickListener(new View.OnClickListener() {
